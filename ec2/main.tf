bb8058b8ebf634a63fc56a5275d2d69a83f91930
Ubuntu <ubuntu@ip-172-31-33-245.eu-central-1.compute.internal>
2020-12-25 05:21:47 +0000

    main.tf has been modified

--- ec2/main.tf
+++ ec2/main.tf
@@ -2,11 +2,22 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type = number
+  default = 8080
+}
+
+output "public_ip" {
+  value = aws_instance.ec2.public_ip 
+  description = "the public IP of the web server"
+}
+
 resource "aws_security_group" "instance" {
   name = "terrraform_WWW"
   ingress {
-    from_port = 80
-    to_port = 80
+    from_port = var.server_port
+    to_port = var.server_port
     protocol = "tcp"
     cidr_blocks = ["178.90.78.180/32"]
   }

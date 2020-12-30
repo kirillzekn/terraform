9a18f55d932ca368f9a9ce1c17eca51b8bc30cb8
Kirill Zelentsov <kzelentsov@tengizchevroil.com>
2020-12-30 17:04:00 +0500

    ec2-main file

--- ec2/main.tf
+++ ec2/main.tf
@@ -2,43 +2,40 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-variable "server_port" {
-  description = "The port the server will use for HTTP requests"
-  type = number
-  default = 80
-}
-
-output "public_ip" {
-  value = aws_instance.ec2.public_ip 
-  description = "the public IP of the web server"
-}
-
-resource "aws_security_group" "instance" {
-  name = "terrraform_WWW"
-  ingress {
-    from_port = var.server_port
-    to_port = var.server_port
-    protocol = "tcp"
-    cidr_blocks = ["178.90.78.180/32"]
-  }
-}
-
-resource "aws_instance" "ec2" {
+resource "aws_instance" "my_web_server" {
   ami = "ami-03c3a7e4263fd998c"
-  instance_type = "t2.micro"
-  key_name = "AWS_FRANKFURT"
-  user_data = <<-EOF
-       #!/bin/bash
-       yum install -y httpd
-       sudo chown -R $USER:$USER /var/www/html
-       echo "Hello World" > /var/www/html/index.html
-       systemctl start httpd.service
-       
-    EOF
-  vpc_security_group_ids = [aws_security_group.instance.id,"sg-0c1c72b767c0f0565"]
-  tags = {
-    Name = "terraform-vm"
-  }
+  instance_type = "t2-micro"
+  vpc_security_group_ids = [aws_security_group.my_web_server.id]
+  user_data = <<EOT
+    #!/usr/bin/bash
+    yum install -y httpd
+    chown -R -v ec2-user /var/www/html/
+    echo "Hello World $(hostname -f) " > /var/www/html/index.html
+    systemctl start httpd  
+  EOT
+  
   
 }
 
+resource "aws_security_group" "my_web_server" {
+  name = "Terraform_Web_Server"
+  description = "my web server group"
+}
+
+resource "aws_security_group_rule" "my_web_server_inbound" {
+  type = "ingress"
+  security_group_id = aws_security_group.my_web_server.id
+  cidr_blocks = [ "178.90.78.180/32" ]
+  protocol = "tcp"
+  to_port = 80
+  from_port = 80
+}
+
+resource "aws_security_group_rule" "my_web_server_outbound" {
+  type = "egress"
+  security_group_id = aws_security_group.my_web_server.id
+  cidr_blocks = [ "0.0.0.0/0" ]
+  protocol = "-1"
+  to_port = 0
+  from_port = 0
+}

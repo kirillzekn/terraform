76ca607c37ac3afcbbcb7145f16840f293bc4b10
Kirill Zelentsov <kzelentsov@tengizchevroil.com>
2021-01-03 17:08:53 +0500

    ASG ELB EC2

--- ELB_ASG_EC2/main.tf
+++ ELB_ASG_EC2/main.tf
@@ -19,15 +19,8 @@ data "aws_availability_zones" "available" {
     state = "available"
 }
 
-data "aws_security_group" "default" {
-filter {
-  name = "group-name"
-  values = ["*Frankfurt_EC2*"]
-}
-}
 
-
-/*
+#/*
 
 #############################################
 #LAUNCH TEMPLATE FOR ASG
@@ -42,9 +35,9 @@ resource "aws_launch_template" "zekn" {
 
   instance_type = "t2.micro"
 
-  vpc_security_group_ids = [data.aws_security_group.default.id]
+  vpc_security_group_ids = [aws_security_group.EC2_to_ELB.id]
 
-  user_data = filebase64("${path.module}/example.sh")
+  user_data = file("user_data.sh")
 }
 
 
@@ -52,18 +45,111 @@ resource "aws_launch_template" "zekn" {
 #ASG
 #############################################
 resource "aws_autoscaling_group" "zekn" {
+  name_prefix = "zekn-"
   availability_zones = [data.aws_availability_zones.available.names[0],data.aws_availability_zones.available.names[1]]
-  desired_capacity   = 1
-  max_size           = 1
-  min_size           = 1
+  desired_capacity   = 2
+  max_size           = 2
+  min_size           = 2
+  health_check_type  = "ELB"
+  target_group_arns =[aws_lb_target_group.zekn.arn]
 
   launch_template {
-    id      = aws_launch_template.foobar.id
+    id      = aws_launch_template.zekn.id
     version = "$Latest"
   }
 }
 
-*/
+#############################################
+#ALB
+#############################################
+resource "aws_lb" "zekn" {
+  name               = "zekn_ALB"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.ELB.id]
+ }
+#############################################
+resource "aws_lb_target_group" "zekn" {
+  name     = "zekn_target_group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id = data.aws_vpc.default.id
+   
+
+  health_check {
+    path = "/"
+    port = 80
+    protocol = "HTTP"
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    interval = 30
+    matcher = "200"  # has to be HTTP 200 or fails
+  }
+}
+#############################################
+resource "aws_alb_listener" "http" {
+  load_balancer_arn = aws_lb.zekn.arn
+  port = 80
+  protocol = "HTTP"
+
+  default_action {
+    target_group_arn = aws_lb_target_group.zekn.arn
+    type = "forward"
+  }
+}
+
+
+#############################################
+#EC2 Security Group
+#############################################
+resource "aws_security_group" "EC2_to_ELB" {
+  name = "EC2_to_ELB"
+}
+
+resource "aws_security_group_rule" "EC2_to_ELB_INGRESS" {
+  type = "ingress"
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
+  source_security_group_id = aws_security_group.ELB.id
+  security_group_id = aws_security_group.EC2_to_ELB.id
+}
+
+resource "aws_security_group_rule" "EC2_to_ELB_EGRESS" {
+  type = "egress"
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  cidr_blocks = [ "0.0.0.0/0" ]
+  security_group_id = aws_security_group.EC2_to_ELB.id
+}
+
+#############################################
+
+resource "aws_security_group" "ELB" {
+  name = "ELB"
+}
+
+resource "aws_security_group_rule" "ELB_INGRESS" {
+  type = "ingress"
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
+  cidr_blocks = [ "178.90.78.180/32" ]
+  security_group_id = aws_security_group.ELB.id
+}
+
+resource "aws_security_group_rule" "ELB_EGRESS" {
+  type = "egress"
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  cidr_blocks = [ "0.0.0.0/0" ]
+  security_group_id = aws_security_group.ELB.id
+}
+
+#*/
 
 #############################################
 #OUTPUT
@@ -74,8 +160,4 @@ output "aws_ami_id" {
 
 output "aws_az" {
     value = data.aws_availability_zones.available.names
-}
-
-output "security_group" {
-    value = data.aws_security_group.default.id
 }

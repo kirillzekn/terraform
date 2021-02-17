52948de0a30f6dfa7213d2af2d09518b11bda336
Kirill Zelentsov <zelentsovkir@gmail.com>
2021-02-17 21:19:52 +0500

    new change

--- 02-Read-Modify-Generate-Config/02-Ref-Cross-Account-Resource-Attribute/02-Ref-Cross-Account-Resource-Attribute.tf
+++ 02-Read-Modify-Generate-Config/02-Ref-Cross-Account-Resource-Attribute/02-Ref-Cross-Account-Resource-Attribute.tf
@@ -2,9 +2,24 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+resource "aws_security_group" "ec2_ssh" {
+  name = "ec2-ssh"
+  description = "allow connection via SSH"
+  egress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["${aws_eip.elastic_ip.public_ip}/32"]
+  }
+}
+
+
 resource "aws_instance" "my_ec2" {
   ami = "ami-0a6dc7529cd559185"
   instance_type = "t2.micro"
+  security_groups =[
+    aws_security_group.ec2_ssh.id
+  ]
 
 }
 
@@ -20,11 +35,11 @@ resource "aws_eip_association" "eip_associate" {
 }
 
 output "instance_id" {
-  value = "aws_instance.my_ec2.instance_id"
+  value = aws_instance.my_ec2.id
 
 }
 
 output "allocation_id" {
-  value = "aws_eip.elastic_ip.id"
+  value = aws_eip.elastic_ip.id
 
 }

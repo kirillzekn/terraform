4c2c527556c93651cd0faebc875f10d8c2934472
Kirill <zelentsovkir@gmail.com>
2021-02-18 22:42:47 +0500

    new fiile has been added

--- 02-Read-Modify-Generate-Config/03-Variables/variables.tf
+++ 02-Read-Modify-Generate-Config/03-Variables/variables.tf
@@ -1,7 +1,13 @@
 variable "my_ip" {
+  type = string
   default = "5.76.1.32/32"
 }
 
 variable "sg_name" {
+  type = string
   default = "my-sec-group"
+}
+
+variable "test_value" {
+  type = number
 }
